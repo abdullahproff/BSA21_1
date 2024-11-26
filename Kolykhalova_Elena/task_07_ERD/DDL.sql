d1a37f3936b3dae3a5c9204690296b9b6b9539e7
elenakolykhalova <94307455+elenakolykhalova@users.noreply.github.com>
2024-11-26 20:08:36 +0300

    ref task_07_end

--- Kolykhalova_Elena/task_07_ERD/DDL.sql
+++ Kolykhalova_Elena/task_07_ERD/DDL.sql
@@ -3,9 +3,10 @@ CREATE TABLE users (
                        user_id SERIAL PRIMARY KEY NOT NULL,
                        username VARCHAR(50) NOT NULL,
                        email VARCHAR(100) UNIQUE NOT NULL,
-                       password VARCHAR(100) NOT NULL,
+                       user_password VARCHAR(100) NOT NULL,
                        interests TEXT[],
-                       location VARCHAR(100),
+                       user_city VARCHAR(100),
+                       user_street VARCHAR(100),
                        profile_picture TEXT,
                        age INT,
                        gender VARCHAR(10),
@@ -15,11 +16,12 @@ CREATE TABLE users (
 -- Создание таблицы активностей
 CREATE TABLE activities (
                             activity_id SERIAL PRIMARY KEY NOT NULL,
-                            owner INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  -- Внешний ключ на пользователя
+                            creator_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,  -- Внешний ключ на пользователя
                             activity_name VARCHAR(100) NOT NULL,
                             activity_date DATE NOT NULL,
                             activity_time TIME NOT NULL,
-                            location VARCHAR(100),
+                            city VARCHAR(100),
+                            street VARCHAR(100),
                             max_participants INT,
                             description TEXT
 );
@@ -33,16 +35,16 @@ CREATE TABLE users_activities (
 
 
 -- Вставка пользователей
-INSERT INTO users (username, email, password, interests, location, profile_picture, age, gender, bio)
+INSERT INTO users (username, email, user_password, interests, user_city, user_street, age, gender, bio)
 VALUES
-    ('john_doe', 'john@example.com', 'password123', ARRAY['велопрогулка', 'фильмы', 'настолки'], 'Москва', 'http://example.com/john.jpg', 30, 'мужской', 'Я люблю активный отдых!'),
-    ('jane_smith', 'jane@example.com', 'securepassword', ARRAY['походы', 'чтение'], 'Санкт-Петербург', 'http://example.com/jane.jpg', 28, 'женский', 'Люблю природу и книги.');
+    ('john_doe', 'john@example.com', 'password123', ARRAY['велопрогулка', 'фильмы', 'настолки'], 'Москва', 'Арбат 1', 30, 'мужской', 'Я люблю активный отдых!'),
+    ('jane_smith', 'jane@example.com', 'securepassword', ARRAY['походы', 'чтение'], 'Санкт-Петербург', 'Невский проспект 5', 28, 'женский', 'Люблю природу и книги.');
 
 -- Вставка активностей
-INSERT INTO activities (owner, activity_name, activity_date, activity_time, location, max_participants, description)
+INSERT INTO activities (creator_id, activity_name, activity_date, activity_time, city, street, max_participants, description)
 VALUES
-    (1, 'Велопрогулка в парке', '2024-07-01', '10:00:00', 'Северный парк', 10, 'Присоединяйтесь к увлекательной велопрогулке!'),
-    (2, 'Киносеанс "Звёздные войны"', '2024-07-02', '19:30:00', 'Кинотеатр "Мир"', 20, 'Посмотрим новый фильм вместе!');
+(1, 'Велопрогулка в парке', '2024-07-01', '10:00:00', 'Москва', 'Северный парк', 10, 'Присоединяйтесь к увлекательной велопрогулке!'),
+(2, 'Киносеанс "Звёздные войны"', '2024-07-02', '19:30:00', 'Санкт-Петербург', 'Кинотеатр "Мир"', 20, 'Посмотрим новый фильм вместе!');
 
 -- Вставка участников
 INSERT INTO users_activities (activity_id, user_id, status)
