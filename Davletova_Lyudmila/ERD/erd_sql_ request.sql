1264ab5329378bc98d9a09c64156dfce0d53b891
Davletova Lyudmila <108341911+luydmila-davletova@users.noreply.github.com>
2024-11-26 18:21:41 +0300

    Create erd_sql_ request.sql

---
+++ Davletova_Lyudmila/ERD/erd_sql_ request.sql
@@ -0,0 +1,32 @@
+-- Просмотр пользователей
+SELECT * FROM users;
+
+-- Просмотр всех треков с указанием их альбомов
+SELECT 
+    tracs.id AS track_id, 
+    tracs.title AS track_title, 
+    albums.title AS album_title, 
+    tracs.duration 
+FROM 
+    tracs
+JOIN 
+    albums ON tracs.album_id = albums.id;
+
+-- Список всех артистов с их жанрами
+SELECT 
+    id AS artist_id, 
+    name AS artist_name, 
+    genre 
+FROM 
+    artists;
+
+-- Список альбомов с артистами, выпустившими эти альбомы
+SELECT 
+    albums.id AS album_id, 
+    albums.title AS album_title, 
+    artists.name AS artist_name, 
+    albums.release_date 
+FROM 
+    albums
+JOIN 
+    artists ON albums.artist_id = artists.id;
