c291b41f21bb0ae3593e1529d3ee091a32a373b9
furstver <furstver@student.21-school.ru>
2024-11-27 21:40:48 +0500

    ERD is created

---
+++ Mukhametyanova_Nargiza/ERD/request.sql
@@ -0,0 +1,20 @@
+SELECT 
+    buyers.surname AS Покупатели,
+    products.product_name AS Товары,
+    best_actions.action_name AS Акции,
+    baskets.basket_status AS Статус_корзины,
+    basket_to_products.quantity AS Количество_товаров,
+    (products.product_cost * basket_to_products.quantity) AS Стоимость_товаров,
+    baskets.basket_cost AS Общая_стоимость_корзины
+FROM 
+    baskets
+JOIN 
+    buyers ON baskets.buyer_id = buyers.buyer_id
+JOIN 
+    basket_to_products ON baskets.basket_id = basket_to_products.basket_id
+JOIN 
+    products ON basket_to_products.product_id = products.product_id
+LEFT JOIN 
+    product_to_actions ON products.product_id = product_to_actions.product_id
+LEFT JOIN 
+    best_actions ON product_to_actions.best_action_id = best_actions.best_action_id;
