8efeb257d25345c1d65fdbb07ed21b6dc77e1621
Anastassia <151686592+Melsfi@users.noreply.github.com>
2024-11-27 21:21:17 +0300

    Update DB.sql

--- Pyatkova_Anastasiya/DB.sql
+++ Pyatkova_Anastasiya/DB.sql
@@ -13,9 +13,7 @@ CREATE TABLE Users (
 CREATE TABLE Products (
     product_id INT PRIMARY KEY,
     product_name VARCHAR,
-	description TEXT,
-    price DECIMAL,
-    available_stock INT
+    price DECIMAL
 );
 
 -- Таблица заказов
@@ -23,11 +21,11 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     user_id INT,
     order_name VARCHAR,
-	main_order INT,
+    main_order VARCHAR,
     order_status VARCHAR,
     total_amount DECIMAL,
-	delivery_status VARCHAR,
-    delivery_date TIMESTAMP,
+    delivery_time_start TIMESTAMP,
+    delivery_time_end TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
@@ -43,17 +41,17 @@ CREATE TABLE Order_Items (
 );
 
 -- Заполнение таблицы Products (продукты)
-INSERT INTO Products (product_id, product_name, price, available_stock) VALUES
-(1, 'Молоко 2.5%', 100, 10),
-(2, 'Вода питьевая', 100, 5),
-(3, 'Груша сезонная', 100, 10),
-(4, 'Огурцы', 200, 5),
-(5, 'Томаты', 200, 80),
-(6, 'Молоко 3.2%', 100, 120),
-(7, 'Вода минеральная', 100, 60),
-(8, 'Яблоко сезонное', 100, 75),
-(9, 'Кабачки', 200, 89),
-(10, 'Киви', 200, 78);
+INSERT INTO Products (product_id, product_name, price) VALUES
+(1, 'Молоко 2.5%', 100),
+(2, 'Вода питьевая', 100),
+(3, 'Груша сезонная', 100),
+(4, 'Огурцы', 200),
+(5, 'Томаты', 200),
+(6, 'Молоко 3.2%', 100),
+(7, 'Вода минеральная', 100),
+(8, 'Яблоко сезонное', 100),
+(9, 'Кабачки', 200),
+(10, 'Киви', 200);
 
 -- Заполнение таблицы Users (пользователи)
 INSERT INTO Users (user_id, user_name, email, phone_number, address) VALUES
@@ -65,11 +63,11 @@ INSERT INTO Users (user_id, user_name, email, phone_number, address) VALUES
 -- Создание заказов
 
 -- Создание основного заказа
-INSERT INTO Orders (order_id, user_id, order_name, order_status, total_amount, delivery_date,  delivery_status) VALUES
-(121253, 1, 'Основной заказ', 'в процессе сборки', 300, '2024-11-27 21:00:00', 'В процессе сборки'),
-(121257, 1, 'Основной заказ', 'передан на доставку', 300, '2024-11-27 21:00:00', 'В процессе доставки'),
-(121258, 1, 'Основной заказ', 'передан на доставку', 300, '2024-11-27 21:00:00', 'В процессе доставки'),
-(121259, 1, 'Основной заказ', 'передан на доставку', 300, '2024-11-27 21:00:00','В процессе доставки');
+INSERT INTO Orders (order_id, user_id, order_name, order_status, total_amount, delivery_time_start, delivery_time_end) VALUES
+(121253, 1, 'Основной заказ', 'в процессе сборки', 300, '2024-11-27 21:00:00', '2024-11-27 21:30:00'),
+(121257, 1, 'Основной заказ', 'в процессе сборки', 300, '2024-11-27 21:00:00', '2024-11-27 21:30:00'),
+(121258, 1, 'Основной заказ', 'в процессе сборки', 300, '2024-11-27 21:00:00', '2024-11-27 21:30:00'),
+(121259, 1, 'Основной заказ', 'в процессе сборки', 300, '2024-11-27 21:00:00', '2024-11-27 21:30:00');
 
 -- Заполнение товаров в основной заказ
 INSERT INTO Order_Items (order_item_id, order_id, product_id, quantity, price) VALUES
@@ -78,8 +76,8 @@ INSERT INTO Order_Items (order_item_id, order_id, product_id, quantity, price) V
 (3, 121253, 3, 1, 100);  -- Груша сезонная
 
 -- Создание дополнительного заказа
-INSERT INTO Orders (order_id, user_id, order_name, order_status, total_amount, delivery_date, main_order) VALUES
-(121253 + 1000, 1, 'Дополнительный заказ', 'в процессе сборки', 500, '2024-11-27 22:00:00', 121253);
+INSERT INTO Orders (order_id, user_id, order_name, order_status, total_amount, delivery_time_start, delivery_time_end, main_order) VALUES
+(121253 + 1000, 1, 'Дополнительный заказ', 'в процессе сборки', 500, '2024-11-27 22:00:00', '2024-11-27 23:00:00', 121253);
 
 -- Заполнение позиций дополнительного заказа
 INSERT INTO Order_Items (order_item_id, order_id, product_id, quantity, price) VALUES
@@ -93,7 +91,6 @@ SELECT o.order_id,
        o.order_name, 
        o.order_status, 
        o.total_amount,
-	   o.delivery_status,
-       main_order.order_id AS main_order_id
+       main_order.order_id AS main_order_id  -- Связь с основным заказом
 FROM Orders o
-LEFT JOIN Orders main_order ON o.main_order = main_order.order_id;  
+LEFT JOIN Orders main_order ON CAST(o.main_order AS INT) = main_order.order_id;
