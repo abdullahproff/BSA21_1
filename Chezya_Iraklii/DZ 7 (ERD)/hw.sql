6c7c782e43072061769cb10d2d867f1ec2decb2b
gloriajeansceo <151613572+gloriajeansceo@users.noreply.github.com>
2024-11-30 18:18:05 +0300

    Update hw.sql

--- Chezya_Iraklii/DZ 7 (ERD)/hw.sql
+++ Chezya_Iraklii/DZ 7 (ERD)/hw.sql
@@ -1,23 +1,27 @@
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
-    fio TEXT NOT NULL
+    fio TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "image" (
     id SERIAL PRIMARY KEY,
     upload_date DATE NOT NULL,
     user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "tag" (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE
+    name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "image_tag" (
     image_id INT NOT NULL,
     tag_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (image_id, tag_id),
     CONSTRAINT fk_image FOREIGN KEY (image_id) REFERENCES "image"(id) ON DELETE CASCADE,
     CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES "tag"(id) ON DELETE CASCADE
