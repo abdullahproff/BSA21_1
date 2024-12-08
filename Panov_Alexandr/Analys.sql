57a3eb088e596dae05df61855b78b32fb7476893
DarkKing656 <112701743+DarkKing656@users.noreply.github.com>
2024-12-08 16:44:53 +0300

    Panov_Erd_try2

--- Panov_Alexandr/Analys.sql
+++ Panov_Alexandr/Analys.sql
@@ -1,7 +1,9 @@
 
+
+
 CREATE TABLE Users (
     name VARCHAR(255) PRIMARY KEY,
-    groups JSONB,  -- Массив имен групп, хранится в формате JSONB
+    groups TEXT,  -- Список групп, хранится в формате текста
     is_premium BOOLEAN,
     email VARCHAR(255),
     phone VARCHAR(20)
@@ -9,28 +11,19 @@ CREATE TABLE Users (
 
 CREATE TABLE Groups (
     group_name VARCHAR(255) PRIMARY KEY,
-    group_users JSONB, -- Массив имен пользователей
-    super_users JSONB, -- Массив имен админов
+    group_users TEXT, -- Список пользователей, хранится в формате текста
+    super_users TEXT, -- Список админов, хранится в формате текста
     group_description TEXT,
-    group_invited JSONB, -- Массив приглашенных пользователей
-    group_blocked JSONB  -- Массив заблокированных пользователей
+    group_invited TEXT, -- Список приглашенных пользователей, хранится в формате текста
+    group_blocked TEXT  -- Список заблокированных пользователей, хранится в формате текста
 );
 
 
-INSERT INTO Users (name, groups, is_premium, email, phone) VALUES 
-('Alice676', '["Group1", "Group2"]', true, 'alice@example.com', '123456789'),
-('Bob44', '["Group1"]', false, 'bob@example.com', '987654321'),
-('Charlie55', '[]', true, 'charlie@example.com', '555555555');
+INSERT INTO Users (name, groups, is_premium, email, phone)
+VALUES 
+    ('Alice45', 'group1;group2;group3', TRUE, 'alice@example.com', '123-456-7890'),
+    ('Bob54', 'group1;group4', FALSE, 'bob@example.com', '123-456-7891'),
+    ('Charlie44', 'group2;group5', TRUE, 'charlie@example.com', '123-456-7892');
 
-INSERT INTO Groups (group_name, group_users, super_users, group_description, group_invited, group_blocked) VALUES 
-('Group1', '["Alice676", "Bob44"]', '["Alice676"]', 'Description of Group 1', '[]', '[]'),
-('Group2', '["Alice676"]', '["Alice676"]', 'Description of Group 2', '["Bob44"]', '[]');
-
-
-
---Вывод списка групп первого пользователя
-SELECT groups::text AS user_groups FROM Users WHERE name = 'Alice676';
-
---Вывод списка админов одной группы
-SELECT super_users::text AS group_admins FROM Groups WHERE group_name = 'Group1';
-
+INSERT INTO Groups (group_name, group_users, super_users, group_description, group_invited, group_blocked)
+VALUES ('group1', 'Alice45;Bob54;Charlie44', 'Alice45', 'Description of group 1', 'Bob54', 'Charlie44');
