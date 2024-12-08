b691ab5af47d5bb260fdd6bdca86b66409dd626b
DarkKing656 <112701743+DarkKing656@users.noreply.github.com>
2024-12-08 18:41:33 +0300

    Panov_last_tty

--- Panov_Alexandr/Analys.sql
+++ Panov_Alexandr/Analys.sql
@@ -1,29 +1,43 @@
-
-
-
+-- Таблица Users
 CREATE TABLE Users (
-    name VARCHAR(255) PRIMARY KEY,
-    groups TEXT,  -- Список групп, хранится в формате текста
-    is_premium BOOLEAN,
-    email VARCHAR(255),
-    phone VARCHAR(20)
+    name VARCHAR(255) PRIMARY KEY,  -- Имя пользователя (ключ)
+    is_premium BOOLEAN,               -- Поле для премиум-статуса
+    email VARCHAR(255),               -- Поле для email
+    phone VARCHAR(20)                 -- Поле для телефона
 );
 
+-- Таблица Groups
 CREATE TABLE Groups (
-    group_name VARCHAR(255) PRIMARY KEY,
-    group_users TEXT, -- Список пользователей, хранится в формате текста
-    super_users TEXT, -- Список админов, хранится в формате текста
-    group_description TEXT,
-    group_invited TEXT, -- Список приглашенных пользователей, хранится в формате текста
-    group_blocked TEXT  -- Список заблокированных пользователей, хранится в формате текста
+    group_name VARCHAR(255) PRIMARY KEY,  -- Имя группы (ключ)
+    group_description TEXT,                -- Описание группы
+    group_settings TEXT                    -- Настройки группы
 );
 
+-- Таблица Relations
+CREATE TABLE Relations (
+    relation_id SERIAL PRIMARY KEY,        -- Уникальный идентификатор для отношения (ключ)
+    user_name VARCHAR(255) REFERENCES Users(name) ON DELETE CASCADE,  -- Внешний ключ на имя пользователя
+    group_name VARCHAR(255) REFERENCES Groups(group_name) ON DELETE CASCADE,  -- Внешний ключ на имя группы
+    relation_type TEXT   -- Тип отношения
+);
 
-INSERT INTO Users (name, groups, is_premium, email, phone)
+-- Вставка пользователей
+INSERT INTO Users (name, is_premium, email, phone)
 VALUES 
-    ('Alice45', 'group1;group2;group3', TRUE, 'alice@example.com', '123-456-7890'),
-    ('Bob54', 'group1;group4', FALSE, 'bob@example.com', '123-456-7891'),
-    ('Charlie44', 'group2;group5', TRUE, 'charlie@example.com', '123-456-7892');
+    ('Alice45', TRUE, 'alice@example.com', '123-456-7890'),
+    ('Bob45', FALSE, 'bob@example.com', '123-456-7891'),
+    ('Charlie44', TRUE, 'charlie@example.com', '123-456-7892');
 
-INSERT INTO Groups (group_name, group_users, super_users, group_description, group_invited, group_blocked)
-VALUES ('group1', 'Alice45;Bob54;Charlie44', 'Alice45', 'Description of group 1', 'Bob54', 'Charlie44');
+-- Вставка групп
+INSERT INTO Groups (group_name, group_description, group_settings)
+VALUES 
+    ('Group A', 'Description of Group A', 'Settings A'),
+    ('Group B', 'Description of Group B', 'Settings B');
+
+-- Вставка отношений
+INSERT INTO Relations (user_name, group_name, relation_type)
+VALUES 
+    ('Alice45', 'Group A', 'admin'),
+    ('Bob45', 'Group A', 'member'),
+    ('Charlie44', 'Group B', 'admin');
+	('Bob45', 'Group B', 'blocked');
